3e84c9db7132bb6e189cffd61496611d55f7dcdb
the T <tina@thes-MacBook-Air.local>
2017-09-12 21:19:09 -0600

    Everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+rank = 1
+genre = ['Classic Rock', 'Pop', 'Jazz', 'Blues', 'Rap']
+100.times do
+    Song.create(
+      title: Faker::Lorem.word,
+      artist: Faker::RockBand.name,
+      rank: rank,
+      genre: genre.sample
+    )
+    rank = rank + 1
+  end
+
+  puts "data has been seeded"
